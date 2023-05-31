1523a342f4cddfbf623e7601a68147527ffdc381
kevit-Meet-Rachhadiya <meet.rachhadiya@kevit.io>
2023-05-31 11:05:33 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const response = await fetch("/api/data");
-    const data = await response.json();
-    setMessage(data.message);
-  };
-
-  return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
-  );
+  // const [message, setMessage] = useState("");
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+  // const fetchData = async () => {
+  //   const response = await fetch("/api/data");
+  //   const data = await response.json();
+  //   setMessage(data.message);
+  // };
+  // return (
+  //   <div className="App">
+  //     <h1>{message}</h1>
+  //   </div>
+  // );
 }
 
 export default App;
